8b4b5f59f38b5a2ee2bb32c5b3b5bed7d099801a
Muhammad Maqsood <mmlilla90@gmail.com>
2020-04-08 13:33:50 +0400

    Code refactorign done for extra codes

--- app/src/main/java/com/maqsood007/weatherforecast/di/module/FragmentModule.kt
+++ app/src/main/java/com/maqsood007/weatherforecast/di/module/FragmentModule.kt
@@ -8,15 +8,11 @@ package com.test.nyt_most_viewed.di.module
 import com.maqsood007.weatherforecast.ui.cities.SelectCitiesFragment
 import com.maqsood007.weatherforecast.ui.forcasts.current_location.ForcastByLocationFragment
 import com.maqsood007.weatherforecast.ui.forcasts.cities.ForecastByCitiesFragment
-import com.maqsood007.weatherforecast.ui.landing.ForecastLandingFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class FragmentModule {
-
-    @ContributesAndroidInjector(modules = [])
-    internal abstract fun contributesForecastLanding(): ForecastLandingFragment
 
     @ContributesAndroidInjector(modules = [])
     internal abstract fun contributesForcastByLocationFragment(): ForcastByLocationFragment
