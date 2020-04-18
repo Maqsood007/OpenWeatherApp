c1cabdded7dfde41ef97900ead903fdf4b79f56c
Muhammad Maqsood <mmlilla90@gmail.com>
2020-04-19 00:49:57 +0400

    - Forecast by location UI tests added.

--- app/src/androidTest/java/com/maqsood007/weatherforecast/ui/ForecastByLocationFragmentTest.kt
+++ app/src/androidTest/java/com/maqsood007/weatherforecast/ui/ForecastByLocationFragmentTest.kt
@@ -2,7 +2,6 @@ package com.maqsood007.weatherforecast.ui
 
 
 import android.content.Context
-import android.util.Log
 import android.view.View
 import androidx.appcompat.view.menu.ActionMenuItem
 import androidx.fragment.app.testing.launchFragmentInContainer
@@ -16,7 +15,6 @@ import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import com.maqsood007.weatherforecast.R
@@ -50,7 +48,14 @@ import java.util.*
 class ForecastByLocationFragmentTest {
 
 
-    @Rule @JvmField var activityRule: ActivityTestRule<MainActivity> = DaggerActivityTestRule<MainActivity>(
+    val todayName = DateTimeUtility.getDayName(
+        Date().toSpecificFormat(DATE_FORMATTER),
+        DATE_FORMATTER
+    )
+
+    @Rule
+    @JvmField
+    var activityRule: ActivityTestRule<MainActivity> = DaggerActivityTestRule<MainActivity>(
         MainActivity::class.java,
         object : OnBeforeActivityLaunchedListener<MainActivity> {
 
@@ -58,17 +63,13 @@ class ForecastByLocationFragmentTest {
                 weatherApp: WeatherApp,
                 activity: MainActivity?
             ) {
-                val forecastAppComponent : WeatherAppTestComponent = DaggerWeatherAppTestComponent
+                val forecastAppComponent: WeatherAppTestComponent = DaggerWeatherAppTestComponent
                     .builder()
                     .application(weatherApp)
                     .build()
 
                 weatherApp.setDaggerForecastComponent(forecastAppComponent = forecastAppComponent)
-
-                Log.d("","")
             }
-
-
         })
 
     @Rule
@@ -77,43 +78,55 @@ class ForecastByLocationFragmentTest {
         GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
 
-//    @Rule
-//    @JvmField
-//    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
-
-
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
     }
 
-    private fun getApp(): WeatherApp? {
-        return InstrumentationRegistry.getInstrumentation()
-            .getTargetContext().getApplicationContext() as WeatherApp
-    }
-
     @Test
-    fun temperatureListContainerIsDisplayed() {
+    fun test_temperatureListContainerIsDisplayed() {
         onView(withId(R.id.recyclerView))
             .check(matches(isDisplayed()))
     }
 
 
     @Test
-    fun today_temprature_textview_visible() {
+    fun test_today_main_info__visible() {
         onView(
             withId(R.id.tvTodayMainTemprature)
         ).check(matches(validateDefaultVisibility(View.VISIBLE)))
-    }
 
-    @Test
-    fun today_temprature_textview_data_binded() {
-        onView(withId(R.id.tvTodayMainTemprature)).check(matches(not(withText(""))))
-    }
+        onView(
+            withId(R.id.tvtodayMainDate)
+        ).check(matches(validateDefaultVisibility(View.VISIBLE)))
 
-    @Test
-    fun today_temprature_img_not_visible() {
+        onView(
+            withId(R.id.tvMainDescription)
+        ).check(matches(validateDefaultVisibility(View.VISIBLE)))
+
+        onView(
+            withId(R.id.tvMainTodayTempRange)
+        ).check(matches(validateDefaultVisibility(View.VISIBLE)))
+
+        onView(
+            withId(R.id.tvMainTodayWinds)
+        ).check(matches(validateDefaultVisibility(View.VISIBLE)))
+
         onView(withId(R.id.imgDescription)).check(matches(validateDefaultVisibility(View.VISIBLE)))
+
+        onView(withId(R.id.imgTempRange)).check(matches(validateDefaultVisibility(View.VISIBLE)))
+
+        onView(withId(R.id.imgWinds)).check(matches(validateDefaultVisibility(View.VISIBLE)))
+    }
+
+
+    @Test
+    fun test_today_main_textview_data_binded() {
+        onView(withId(R.id.tvTodayMainTemprature)).check(matches(not(withText(""))))
+        onView(withId(R.id.tvtodayMainDate)).check(matches(not(withText(""))))
+        onView(withId(R.id.tvMainDescription)).check(matches(not(withText(""))))
+        onView(withId(R.id.tvMainTodayTempRange)).check(matches(not(withText(""))))
+        onView(withId(R.id.tvMainTodayWinds)).check(matches(not(withText(""))))
     }
 
     private fun validateDefaultVisibility(visibility: Int): Matcher<View> {
@@ -132,28 +145,107 @@ class ForecastByLocationFragmentTest {
     }
 
     @Test
-    fun testToday_name_displayer_first_row() {
+    fun testToday_name_displayed_first_row() {
         onView(withId(R.id.recyclerView))
             .check(
                 matches(
                     hasTodayNameForGivenPosition(
                         0,
-                        DateTimeUtility.getDayName(
-                            Date().toSpecificFormat(DATE_FORMATTER),
-                            DATE_FORMATTER
-                        )
+                        todayName
                     )
                 )
             )
 
     }
 
+
+    @Test
+    fun test_second_list_item_has_correct_name_wrt_today() {
+        onView(withId(R.id.recyclerView))
+            .check(
+                matches(
+                    hasTodayNameForGivenPosition(
+                        1,
+                        getMappedDays(todayName, 1)
+                    )
+                )
+            )
+
+    }
+
+    @Test
+    fun test_third_list_item_has_correct_name_wrt_today() {
+        onView(withId(R.id.recyclerView))
+            .check(
+                matches(
+                    hasTodayNameForGivenPosition(
+                        2,
+                        getMappedDays(todayName, 2)
+                    )
+                )
+            )
+
+    }
+
+
+    @Test
+    fun test_fourth_list_item_has_correct_name_wrt_today() {
+        onView(withId(R.id.recyclerView))
+            .check(
+                matches(
+                    hasTodayNameForGivenPosition(
+                        3,
+                        getMappedDays(todayName, 3)
+                    )
+                )
+            )
+
+    }
+
+    @Test
+    fun test_fifth_list_item_has_correct_name_wrt_today() {
+        onView(withId(R.id.recyclerView))
+            .check(
+                matches(
+                    hasTodayNameForGivenPosition(
+                        4,
+                        getMappedDays(todayName, 4)
+                    )
+                )
+            )
+
+    }
+
+    @Test
+    fun test_sixth_list_item_has_correct_name_wrt_today() {
+        onView(withId(R.id.recyclerView))
+            .check(
+                matches(
+                    hasTodayNameForGivenPosition(
+                        5,
+                        getMappedDays(todayName, 5)
+                    )
+                )
+            )
+
+    }
+
+
+
+    @Test
+    fun test_main_temp_always_equal_list_zero_index_temperature(){
+
+
+    }
+
+
+
     private fun hasTodayNameForGivenPosition(position: Int, today: String): Matcher<View> {
 
         val recyclerBounderMatcher =
             object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
                 override fun describeTo(description: Description?) {
-                    description?.appendText("Is today name displayed")
+                    description?.appendText("Is day name displayed correctly")
                 }
 
                 override fun matchesSafely(recyclerView: RecyclerView?): Boolean {
@@ -181,6 +273,86 @@ class ForecastByLocationFragmentTest {
 
     @After
     fun tearDown() {
+
+    }
+
+
+    private fun getMappedDays(dayName: String, position: Int): String {
+
+        when (dayName) {
+
+            "Saturday" -> {
+                when (position) {
+                    0 -> return "Saturday"
+                    1 -> return "Sunday"
+                    2 -> return "Monday"
+                    3 -> return "Tuesday"
+                    4 -> return "Wednesday"
+                    else -> return "Thursday"
+                }
+            }
+            "Sunday" -> {
+                when (position) {
+                    0 -> return "Sunday"
+                    1 -> return "Monday"
+                    2 -> return "Tuesday"
+                    3 -> return "Wednesday"
+                    4 -> return "Thursday"
+                    else -> return "Friday"
+                }
+            }
+            "Monday" -> {
+                when (position) {
+                    0 -> return "Monday"
+                    1 -> return "Tuesday"
+                    2 -> return "Wednesday"
+                    3 -> return "Thursday"
+                    4 -> return "Friday"
+                    else -> return "Saturday"
+                }
+            }
+            "Tuesday" -> {
+                when (position) {
+                    0 -> return "Tuesday"
+                    1 -> return "Wednesday"
+                    2 -> return "Thursday"
+                    3 -> return "Friday"
+                    4 -> return "Saturday"
+                    else -> return "Sunday"
+                }
+            }
+            "Wednesday" -> {
+                when (position) {
+                    0 -> return "Wednesday"
+                    1 -> return "Thursday"
+                    2 -> return "Friday"
+                    3 -> return "Saturday"
+                    4 -> return "Sunday"
+                    else -> return "Monday"
+                }
+            }
+            "Thursday" -> {
+                when (position) {
+                    0 -> return "Thursday"
+                    1 -> return "Friday"
+                    2 -> return "Saturday"
+                    3 -> return "Sunday"
+                    4 -> return "Monday"
+                    else -> return "Tuesday"
+                }
+            }
+            else -> {
+                when (position) {
+                    0 -> return "Friday"
+                    1 -> return "Saturday"
+                    2 -> return "Sunday"
+                    3 -> return "Monday"
+                    4 -> return "Tuesday"
+                    else -> return "Wednesday"
+                }
+            }
+
+        }
 
     }
 
